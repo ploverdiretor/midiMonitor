3da61882a9354722f4105c90d82582a77066538d
plover <merryplover@gmail.com>
2026-03-08 21:35:45 +0900

    フィルター編集

--- osc.js
+++ osc.js
@@ -14,14 +14,19 @@
   const tablen = 8;
   const real = new Float32Array(tablen);
   const imag = new Float32Array(tablen);
-
+  const vcaGain = new GainNode(audioctx);
+  /*-----------------------------フィルター------------------------------*/
+  var filter = []
+  for (let i=0;i<16;i++){
+  filter[i] = new BiquadFilterNode(audioctx,{frequency:5000, q:5});
+  }
   /*-----------------------------音量------------------------------*/
   const master = new GainNode(audioctx);
   
   /*-----------------------------接続------------------------------*/
   for (let i=0;i<16;i++){
     osc[i].connect(gain[i]).connect(master)
-    vco[i].connect(vca[i]).connect(master)
+    vco[i].connect(vca[i]).connect(filter[i]).connect(vcaGain).connect(master)
   }
   master.connect(audioctx.destination);
   /*-----------------------------初期化------------------------------*/
@@ -48,10 +53,15 @@
     });
   }
   document.getElementById("master").addEventListener("input", Setup);
+  document.getElementById("vcaGain").addEventListener("input", SetupVCO);
   for (let i=0;i<16;i++){
     osc[i].frequency.value = voiceFreq[i];
     vco[i].frequency.value = voiceFreq[i];
   }
+  document.getElementById("typeFilter").addEventListener("change", SetupVCO);
+  document.getElementById("freqF").addEventListener("input", SetupVCO);
+  document.getElementById("qF").addEventListener("input", SetupVCO);
+  document.getElementById("gainF").addEventListener("input", SetupVCO);
   for (let j=0;j<16;j++){
     SetupWave(j);
   }
@@ -70,6 +80,13 @@
     for (let i=0;i<16;i++){
       vca[i].gain.value = vcaLevel[i];
       vco[i].type = type;
+    }
+    vcaGain.gain.value = document.getElementById("vcaVal").innerHTML = document.getElementById("vcaGain").value;
+    for (let i=0;i<16;i++){
+      filter[i].type = ["lowpass","highpass","bandpass","lowshelf","highshelf","peaking","notch","allpass"][document.getElementById("typeFilter").selectedIndex];
+      filter[i].frequency.value = document.getElementById("freqvalFilter").innerHTML = document.getElementById("freqF").value;
+      filter[i].Q.value = document.getElementById("qvalFilter").innerHTML = document.getElementById("qF").value;
+      filter[i].gain.value = document.getElementById("gainvalFilter").innerHTML = document.getElementById("gainF").value;
     }
 }
   /*-----------------------------ドローバー設定------------------------------*/
